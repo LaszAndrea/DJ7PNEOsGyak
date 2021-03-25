73d5445e12cebf19930c2ec2f1103dbe58e7a610
LaszAndrea <79246573+LaszAndrea@users.noreply.github.com>
2021-03-25 09:03:27 +0100

    Add files via upload

---
+++ DJ7PNE_0325/DJ7PNE_openclose.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/file.h>
+#include <sys/types.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+
+int main()
+{
+    int p;
+    int elozo;
+    char buff[100];
+    buff[0] = 0;
+
+    p = open("DJ7PNE.txt", O_RDWR);
+
+    if (p==-1)
+    {
+        perror("Fajl megnyitasa soran [open()] hiba lepett fel!\n");
+        exit(-1);
+    }
+    else
+    {
+        printf("A fajlt megnyitottam olvasasra\n");
+    }
+
+    elozo = read(p, buff, 50);
+    printf("Beolvasott uzenet %s\n", buff);
+    printf("A read() %d byteot olvasott.\n", elozo);
+    strcpy(buff, "DJ7PNE");
+
+    elozo = lseek(p, 0, SEEK_SET);
+    printf("Az lseek() beallt a %d. helyre", elozo);
+
+    elozo = write(p, buff, 6);
+    printf("\nIde a write() %d byteot irt: %s\n", elozo, buff);
+    close(p);
+
+    return 0;
+}
